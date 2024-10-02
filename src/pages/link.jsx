7685dd013e744cd172f53b9cfe37782fcdb1259b
Piyush Rathwe <110498743+piyush-3020@users.noreply.github.com>
2024-10-02 21:06:49 +0530

    Update link

--- src/pages/link.jsx
+++ src/pages/link.jsx
@@ -76,7 +76,7 @@ const LinkPage = () => {
             {url?.title}
           </span>
           <a
-            href={`https://zap-url.onrender.com/${link}`}
+            href={`/${link}`}
             target="_blank"
             className="text-3xl sm:text-4xl text-blue-400 font-bold hover:underline cursor-pointer"
           >
